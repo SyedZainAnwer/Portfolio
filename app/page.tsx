a70cb0e1383ee37223f8f37fc1a1eb33273a1c9c
Syed Zain Anwer <zainanwer24@gmail.com>
2023-12-24 23:51:27 +0100

    created about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import About from "@/components/About";
 import Intro from "@/components/Intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex items-center flex-col px-4">
       <Intro />
+      <SectionDivider />
+      <About />
     </main>
   )
 }
---
+++ components/Section-Heading.tsx
@@ -0,0 +1,7 @@
+const SectionHeading = ({heading}: {heading: string}) => {
+    return (
+        <h2 className="text-3xl font-medium capitalize mb-8">{heading}</h2>
+    )
+}
+
+export default SectionHeading;
